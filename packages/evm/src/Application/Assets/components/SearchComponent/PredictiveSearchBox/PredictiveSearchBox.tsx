9dbd2d8f0d9d255632897ad4548b90606a410563
Muhammad Jaffar Raza <muhammad.raza@irsavideo.com>
2021-05-03 13:40:28 +0500

    consoles removed. Worker code cleaned.

--- packages/evm/src/Application/Assets/components/SearchComponent/PredictiveSearchBox/PredictiveSearchBox.tsx
+++ packages/evm/src/Application/Assets/components/SearchComponent/PredictiveSearchBox/PredictiveSearchBox.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import "./PredictiveSearchBox.scss";
 import SearchWorker from "../../../utils/search.worker";
+import useSearchWorker from "../../../utils/useSearchWorker";
 import Outcome from "./Outcome";
 
 interface Props {
@@ -14,7 +15,6 @@ const PredictiveSearchBox: React.FC<Props> = ({ children, onSet }) => {
     };
   }, []);
   const [searchData, setSearchData] = useState<any>();
-  const [searchQuerry, setSearchQuerry] = useState<any>();
   const [showSearch, setShowSearch] = useState<any>(false);
   const [outCome, setOutCome] = useState<any>([]);
   const [inputValue, setInputValue] = useState<string>("");
@@ -27,8 +27,7 @@ const PredictiveSearchBox: React.FC<Props> = ({ children, onSet }) => {
     onSet(value);
 
     const worker: Worker = new SearchWorker();
-    worker.postMessage({ value });
-
+    //message recieved from worker.
     worker.addEventListener(
       "message",
       function (e) {
@@ -37,7 +36,6 @@ const PredictiveSearchBox: React.FC<Props> = ({ children, onSet }) => {
       false
     );
 
-    setSearchQuerry(value);
     if (value.length >= 3) {
       const data = await fetchData();
 
@@ -53,7 +51,7 @@ const PredictiveSearchBox: React.FC<Props> = ({ children, onSet }) => {
       must: [
         {
           query_string: {
-            query: `${searchQuerry}*`,
+            query: `${inputValue}*`,
             fields: [
               "asset.assetName",
               "categories",
