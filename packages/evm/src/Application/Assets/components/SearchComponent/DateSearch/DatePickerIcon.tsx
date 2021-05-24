d03cf808dfe8188588fd95e62c79462c9ace39f1
Muhammad Jaffar Raza <muhammad.raza@irsavideo.com>
2021-05-24 16:17:22 +0500

    clear all option added

--- packages/evm/src/Application/Assets/components/SearchComponent/DateSearch/DatePickerIcon.tsx
+++ packages/evm/src/Application/Assets/components/SearchComponent/DateSearch/DatePickerIcon.tsx
@@ -38,6 +38,7 @@ const DatePickerIcon: React.FC<Props> = ({ onClose, dropDownCustomValue }) => {
     setStartDateValue("");
     setEndDateValue("");
     setSelectedOption("");
+    dropDownCustomValue("clearAll")
   };
 
   return (
