8121b1337ab90e2757194f3263cda6db99c7852f
Mohsin Memon <mmemon@irsavideo.com>
2021-06-15 13:22:58 +0000

    Merged in B_Temp_Date_Time_Constants_Fixes (pull request #17)
    B Temp Date Time Constants Fixes
    
    * date time constants updated.

--- packages/evm/src/utils/constant.ts
+++ packages/evm/src/utils/constant.ts
@@ -5,45 +5,45 @@ const basicDateOptions = [
     { 
       value: "today", 
       displayText: "today",  
-      startDate : function(){ return moment().startOf("day").format() }, 
+      startDate : function(){ return moment().startOf("day").set("second", 0).format() }, 
       endDate : function(){ return   moment().endOf("day").set("second", 0).format() } 
     },
     { 
       value: "yesterday", 
       displayText: " yesterday" ,
-      startDate : function(){ return moment().startOf("day").subtract(1, "days").format() }, 
-      endDate : function(){ return   moment().endOf("day").subtract(1, "days").format() } 
+      startDate : function(){ return moment().startOf("day").subtract(1, "days").set("second", 0).format() }, 
+      endDate : function(){ return   moment().endOf("day").subtract(1, "days").set("second", 0).format() } 
       
     },
     {
       value: "last 7 days",
       displayText: "last 7 days" ,
-      startDate : function(){ return moment().startOf("day").subtract(7, "days").format() }, 
-      endDate : function(){ return   moment().endOf("day").subtract(1, "days").set("second", 0).format()} 
+      startDate : function(){ return moment().startOf("day").subtract(6, "days").set("second", 0).format() }, 
+      endDate : function(){ return   moment().endOf("day").set("second", 0).format()} 
     },     
     { 
       value: "last 30 days", 
       displayText: "last 30 days" ,
-      startDate : function(){ return moment().startOf("day").subtract(30, "days").format() }, 
+      startDate : function(){ return moment().startOf("day").subtract(29, "days").set("second", 0).format() }, 
       endDate : function(){ return   moment().endOf("day").set("second", 0).format() } 
     },
     { 
       value: "current calendar month", 
       displayText: "current calendar month" ,
-      startDate : function(){ return moment().startOf("month").format() }, 
-      endDate : function(){ return  moment().format()}  
+      startDate : function(){ return moment().startOf("month").set("second", 0).format() }, 
+      endDate : function(){ return  moment().endOf("month").set("second", 0).format()}  
     },
     { 
       value: "last calendar month", 
       displayText: "last calendar month" ,
-      startDate : function(){ return moment().subtract(1, "months").endOf("month").format() }, 
-      endDate : function(){ return   moment().startOf("month").format() } 
+      startDate : function(){ return moment().subtract(1, "months").startOf("month").set("second", 0).format() }, 
+      endDate : function(){ return   moment().subtract(1, "months").endOf("month").set("second", 0).format() } 
     },
     { 
       value: "custom", 
       displayText: "custom range" , 
-      startDate :function(){ return  moment().format()} , 
-      endDate :  function(){ return  moment().format()} 
+      startDate :function(){ return  moment().set("second", 0).format()} , 
+      endDate :  function(){ return  moment().set("second", 0).format()} 
     },
 ];
 
@@ -53,15 +53,15 @@ const approachingDeletionDateOptions = [
     value: "tomorrow", 
     displayText: "tomorrow", 
     current:  moment() , 
-    startDate : function(){ return moment().startOf("day").add(1, "days").format() }, 
-    endDate : function(){ return   moment().endOf("day").add(1, "days").set("second",59).format() }
+    startDate : function(){ return moment().startOf("day").add(1, "days").set("second",0).format() }, 
+    endDate : function(){ return   moment().endOf("day").add(1, "days").set("second",0).format() }
   },
   { 
     value: "next 7 days", 
     displayText: " next 7 day" ,
     current:  moment() , 
     startDate : function(){ return moment().startOf("day").add(1, "days").set("second", 0).format()  }, 
-    endDate : function(){ return moment().endOf("day").add(7, "days").set("second",59).format()  } 
+    endDate : function(){ return moment().endOf("day").add(7, "days").set("second",0).format()  } 
     
   },
   {
@@ -69,7 +69,7 @@ const approachingDeletionDateOptions = [
     displayText: "next 30 days",
     current:  moment() , 
     startDate : function(){ return moment().startOf("day").add(1,"days").set("second", 0).format()  }, 
-    endDate : function(){ return moment().endOf("day").add(30, "days").set("second",59).format()   } 
+    endDate : function(){ return moment().endOf("day").add(30, "days").set("second",0).format()   } 
   }
 ];
 
