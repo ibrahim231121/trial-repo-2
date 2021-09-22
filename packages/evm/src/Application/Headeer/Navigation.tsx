089dc976b528746bafff4e908092ad774560ef1c
Muhammad Waseem <muhammad.waseem@irsavideo.com>
2021-09-22 09:31:51 +0000

    Merged in CRX-2535_Permissions-Admin_Menu_Dropdown_option (pull request #85)
    CRX-2535-change setups to admin in main menu
    
    * change setups to admin in main menu
    
    * Merged Feature_Asset_Lister into CRX-2535_Permissions-Admin_Menu_Dropdown_option
    
    Approved-by: Mohsin Memon

--- packages/evm/src/Application/Headeer/Navigation.tsx
+++ packages/evm/src/Application/Headeer/Navigation.tsx
@@ -4,211 +4,181 @@ import './Navigation.scss'
 
 const CRXLefNavigation = () => {
    const history = useHistory();
-   const navigateToPage = (path : string) => {
-		history.push(path);
-	}
+   const navigateToPage = (path: string) => {
+      history.push(path);
+   }
 
-    const items = [
-        {
-           label:'Home',
-           icon:'fas fa-chart-pie NaveIcon',
-        },
-        {
-           label:'Assets',
-           icon:'icon-file-video NaveIcon',
-           url : "/assets",
-           items:[
-              {
-                 label:'Assets',
-                 command:()=>{ navigateToPage("/assets")},
-              },
-              {
-                 label:'Right',
-                 
-              },
-              {
-                 label:'Center',
-                 
-              },
-              {
-                 label:'Justify',
-                 
-              },
-    
-           ]
-        },
-        {
-           label:'Cases',
-           icon:'fas fa-briefcase NaveIcon',
-           items:[
-              {
-                 label:'New',
-                
-    
-              },
-              {
-                 label:'Delete',
-                 
-    
-              },
-              {
-                 label:'Search',
-                 
-                 items:[
-                    {
-                       label:'Filter',
-                       
-                       items:[
-                          {
-                             label:'Print',
-                             
-                          }
-                       ]
-                    },
-                    {
-                       
-                       label:'List'
-                    }
-                 ]
-              }
-           ]
-        },
-        {
-           label:'Live Video',
-           icon:'fas fa-video NaveIcon',
-           classes: 'liveVideoTab',
-           items:[
-              {
-                 label:'Edit',
-                 
-                 items:[
-                    {
-                       label:'Save',
-                       
-                    },
-                    {
-                       label:'Delete',
-                       
-                    }
-                 ]
-              },
-              {
-                 label:'Archieve',
-                 
-                 items:[
-                    {
-                       label:'Remove',
-                       
-                    }
-                 ]
-              }
-           ]
-        },
-        {
-            label:'Maps',
-            icon:'icon-compass2 NaveIcon',
-            classes : 'mapsTab',
-            items:[
-               {
-                  label:'Analytics Map',
-                  
-               },
-               {
-                  label:'AVL Map',
-                  
-               },
-            ]
-        },
+   const items = [
+      {
+         label: 'Home',
+         icon: 'fas fa-chart-pie NaveIcon',
+      },
+      {
+         label: 'Assets',
+         icon: 'icon-file-video NaveIcon',
+         url: "/assets",
+         items: [
+            {
+               label: 'Assets',
+               command: () => { navigateToPage("/assets") },
+            },
+            {
+               label: 'Right',
 
-        {
-            label:'ALPR',
-            icon:'fas fa-address-card NaveIcon',
-            classes : 'aplrTab',
-            items:[
-                {
-                  label:'Plate Captures',
-                },
-                {
-                  label:'Live ALPR',
-                },
-                {
-                    label:'Live ALPR',
-                },
-                {
-                    label : 'Manage Hot List'
-                },
-                {
-                    separator:true
-                },
-                {
-                    label : 'Manage Hot List Data Source'
-                },
-                {
-                    label : 'Manage License Plates'
-                }
-            ]
-        },
+            },
+            {
+               label: 'Center',
 
-        {
-            label:'Reports',
-            icon:'icon-file-text NaveIcon',
-            classes : 'reportingTab',
-            items:[
-               {
-                  label:'Analytics Map',
-               },
-               {
-                  label:'AVL Map',
-               },
-            ]
-        },
+            },
+            {
+               label: 'Justify',
 
-        {
-            label:'Setups',
-            icon:'fas fa-cogs NaveIcon',
-            items:[
-                {
-                    label:'System Settings',
-                },
-                {
-                    label:'Unit Configuration',
-                },
-                {
-                    label:'Policies',
-                },
-                {
-                    label:'Permissions',
-                },
-                {
-                    label:'Category Forms',
-                },
-                {
-                    label:'Active Directory',
-                },
-                {
-                    label:'CAD Integration',
-                },
-                {
-                    label:'Versions',
-                },
-                {
-                    label : 'Unit Update'
-                },
-                {
-                    label : 'Storage'
-                },
-                {
-                    label : 'RTCC'
-                }
+            },
 
-            ]
-        },
+         ]
+      },
+      {
+         label: 'Cases',
+         icon: 'fas fa-briefcase NaveIcon',
+         items: [
+            {
+               label: 'New',
 
-    ];
-    
-    return (
-        <CRXNestedMenu className="CRXLeftMenu" model={items} />
-    )
+
+            },
+            {
+               label: 'Delete',
+
+
+            },
+            {
+               label: 'Search',
+
+               items: [
+                  {
+                     label: 'Filter',
+
+                     items: [
+                        {
+                           label: 'Print',
+
+                        }
+                     ]
+                  },
+                  {
+
+                     label: 'List'
+                  }
+               ]
+            }
+         ]
+      },
+      {
+         label: 'Live Video',
+         icon: 'fas fa-video NaveIcon',
+         classes: 'liveVideoTab',
+         items: [
+            {
+               label: 'Edit',
+
+               items: [
+                  {
+                     label: 'Save',
+
+                  },
+                  {
+                     label: 'Delete',
+
+                  }
+               ]
+            },
+            {
+               label: 'Archieve',
+
+               items: [
+                  {
+                     label: 'Remove',
+
+                  }
+               ]
+            }
+         ]
+      },
+      {
+         label: 'Maps',
+         icon: 'icon-compass2 NaveIcon',
+         classes: 'mapsTab',
+         items: [
+            {
+               label: 'Analytics Map',
+
+            },
+            {
+               label: 'AVL Map',
+
+            },
+         ]
+      },
+
+      {
+         label: 'ALPR',
+         icon: 'fas fa-address-card NaveIcon',
+         classes: 'aplrTab',
+         items: [
+            {
+               label: 'Plate Captures',
+            },
+            {
+               label: 'Live ALPR',
+            },
+            {
+               label: 'Live ALPR',
+            },
+            {
+               label: 'Manage Hot List'
+            },
+            {
+               separator: true
+            },
+            {
+               label: 'Manage Hot List Data Source'
+            },
+            {
+               label: 'Manage License Plates'
+            }
+         ]
+      },
+
+      {
+         label: 'Reports',
+         icon: 'icon-file-text NaveIcon',
+         classes: 'reportingTab',
+         items: [
+            {
+               label: 'Analytics Map',
+            },
+            {
+               label: 'AVL Map',
+            },
+         ]
+      },
+
+      {
+         label: 'Admin',
+         icon: 'fas fa-sitemap NaveIcon',
+         items: [
+            {
+               label: 'User Groups'
+            }
+
+         ]
+      },
+
+   ];
+
+   return (
+      <CRXNestedMenu className="CRXLeftMenu" model={items} />
+   )
 }
 
 export default CRXLefNavigation;
