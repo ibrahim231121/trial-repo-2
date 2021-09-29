8ef1e4d025dd85d3bb89aa739e60e335d84bcd31
Muhammad Jaffar Raza <muhammad.raza@irsavideo.com>
2021-09-29 13:42:18 +0500

    done.

--- packages/evm/src/Application/Headeer/Breadcrumb.tsx
+++ packages/evm/src/Application/Headeer/Breadcrumb.tsx
@@ -7,11 +7,39 @@ import { useSelector, useDispatch } from "react-redux";
 import { enterPathActionCreator } from "../../Redux/breadCrumbReducer";
 
 const ActiveBreadcrumb: React.FC<any> = React.memo((props) => {
+  const ref:any = React.useRef()
   const dispatch = useDispatch();
   const pathLastState = useSelector((state: any) => state.pathName);
+  const [width, setWidth] = React.useState<number>(window.innerHeight);
+
   const {
     location: { pathname },
   } = props;
+
+  function debounce(fn:()=>void, ms:number) {
+    let timer:any=null
+    return () => {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        timer = null
+        fn()
+      }, ms)
+    };
+  }
+
+
+  React.useEffect(() => {
+    const h= ref.current
+    const debouncedEvent= debounce(function handleResize() {
+      setWidth(window.innerWidth);
+    },1000)
+
+    window.addEventListener("resize", debouncedEvent);
+    return ()=> {
+      window.removeEventListener('resize', debouncedEvent)
+    
+}
+  });
 
   const pathnames = pathname.split("/").filter((x: any) => x);
   const checkPath = (isLast: any, name: string, routeTo: string) => {
@@ -55,15 +83,15 @@ const ActiveBreadcrumb: React.FC<any> = React.memo((props) => {
         })
       }
     >
-      <CRXBreadcrumb  maxItems={3}>
-        <Link className="brdLinks breadCrumbItem" to="/">
+      <CRXBreadcrumb maxItems={width <= 650 ? 3 : 100}>
+        <Link className="brdLinks breadCrumbItem" to="/" >
           Home
         </Link>
         {pathnames.map((name: any, index: number) => {
           const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
           const isLast = index === pathnames.length - 1;
           name = name.charAt(0).toUpperCase() + name.slice(1);
-          return <span key={name}>{checkPath(isLast, name, routeTo)}</span>;
+          return <span key={name} ref={ref}>{checkPath(isLast, name, routeTo)}</span>;
         })}
       </CRXBreadcrumb>
       <CRXTitle
