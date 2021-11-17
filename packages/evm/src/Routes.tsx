ac5c4435582b7c78ea1c70e9cd66bc0b1dae9084
osama toufiq <osama.toufiq@hyperdatacomputing.com>
2021-11-17 10:14:44 +0000

    Merged in B-CRX-2769-Footer-position-should-not-be-fixed (pull request #130)
    B CRX-2769 Footer position should not be fixed
    
    * Done
    
    * Done
    
    
    Approved-by: Sajid Hussain

--- packages/evm/src/Routes.tsx
+++ packages/evm/src/Routes.tsx
@@ -31,7 +31,7 @@ const Routes = () => {
             <AppHeader onClick={handleDrawerToggle} onClose={handleDrawerToggle} open={open} />
           </CRXAppBar>
           <main
-            className={clsx(classes.content, {
+            className={clsx(classes.content,'crx-main-container', {
               [classes.contentShift]: open,
             })}
           >
