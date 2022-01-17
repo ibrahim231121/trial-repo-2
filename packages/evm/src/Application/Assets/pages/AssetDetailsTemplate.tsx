d7878f8fef657fcadc023953b63ae7dbcd46f19b
Muhammad  Bilal <mbilal@irsavideo.com>
2022-01-17 07:22:55 +0000

    Merged in CRX-A-2929-Asset-Detail-Page-Tabs (pull request #201)
    CRX-2929 Asset Detail Page Tabs
    
    * Tabs Component added
    
    * Merged Feature/Unit_Configuration into CRX-A-2929-Asset-Detail-Page-Tabs

--- packages/evm/src/Application/Assets/pages/AssetDetailsTemplate.tsx
+++ packages/evm/src/Application/Assets/pages/AssetDetailsTemplate.tsx
@@ -1,16 +1,48 @@
 import React, { useEffect } from 'react'
+import { CRXTabs, CrxTabPanel, CRXRows, CRXColumn } from "@cb/shared";
 
-
-const AssetDetailsTemplate= (props:any) => {
-
-    useEffect(()=>{
+const AssetDetailsTemplate = (props: any) => {
+    const [value, setValue] = React.useState(0);
+    function handleChange(event: any, newValue: number) {
+        setValue(newValue);
+    }
+    useEffect(() => {
         document.title += " - " + props.match.params.id
-    },[])
+    }, [])
+    const tabs = [
+        { label: "Map", index: 0 },
+        { label: "Related Assets", index: 1 },
+    ];
 
     return (
-        <div style={{margin:'225px'}}>
+        <div style={{ margin: '225px' }}>
             Asset Detail Page
+
+            <CRXRows container spacing={0}>
+                <CRXColumn item xs={8} className='topColumn'>
+                    <div>
+                        Video Player Container
+                    </div>
+                </CRXColumn>
+                <CRXColumn item xs={4} className='topColumn'>
+                    <div className="tabCreateTemplate">
+                        <CRXTabs value={value} onChange={handleChange} tabitems={tabs} />
+                        <div className="tctContent">
+                            <CrxTabPanel value={value} index={0}  >
+                                <div>Map</div>
+                            </CrxTabPanel>
+
+                            <CrxTabPanel value={value} index={1}  >
+                                <div>Test</div>
+                            </CrxTabPanel>
+                        </div>
+                    </div>
+                </CRXColumn>
+            </CRXRows>
+
+
         </div>
+
     )
 }
 
