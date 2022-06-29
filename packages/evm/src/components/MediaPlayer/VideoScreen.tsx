48ae7b8018a83f304c8e60b30d1def1d37a59a97
osama toufiq <osama.toufiq@hyperdatacomputing.com>
2022-06-29 15:35:03 +0000

    Merged in VD-GEP-231-Play-and-Pause-Feedback-Visual-Design (pull request #417)
    VD GEP-231 Play and Pause Feedback Visual Design
    
    * Done
    
    * css issues fix
    
    * env file issue fix
    
    * VD-GEP-231-Play-and-Pause-Feedback-Visual-Design
    
    * VD-GEP-231-Play-and-Pause-Feedback-Visual-Design
    
    * GEP-231 Play pasue button
    
    * Merge remote-tracking branch 'remotes/origin/development' into VD-GEP-231-Play-and-Pause-Feedback-Visual-Design
    
    # Conflicts:
    #	packages/evm/environment/.env.dev
    #	packages/evm/src/components/MediaPlayer/VideoPlayer.scss
    #	packages/evm/src/components/Media…
    * build issue resolved
    
    * build issues
    
    * Merged development into VD-GEP-231-Play-and-Pause-Feedback-Visual-Design
    
    Approved-by: Sajid Hussain

--- packages/evm/src/components/MediaPlayer/VideoScreen.tsx
+++ packages/evm/src/components/MediaPlayer/VideoScreen.tsx
@@ -1,11 +1,12 @@
-import React, { useState , useRef } from "react";
-import Grid from '@material-ui/core/Grid';
+import React, { useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
 import VideoPlayerFastFwRw from "./VideoPlayerFastFwRw";
 import { Menu, MenuButton, MenuItem } from "@szhsin/react-menu";
 import VideosSelection from "./VideosSelection";
 import { Switch } from "@material-ui/core";
-import AssetDetailsDropdown from "../../../src/Application/Assets/Detail/AssetDetailsDropdown"
-import { Style } from "@material-ui/icons";
+import "../../Assets/css/animate.min.css"
+import AssetDetailsDropdown from "../../../src/Application/Assets/Detail/AssetDetailsDropdown";
+
 
 interface VideoScreenProp {
   viewNumber?: number,
@@ -33,41 +34,57 @@ interface VideoScreenProp {
 const VideoScreen = ({onClickBookmarkNote,isPlaying, viewNumber, timelinedetail, settimelinedetail, mapEnabled, videoHandlers, setVideoHandlersFwRw, setvideoTimerFwRw, onClickVideoFwRw, isOpenWindowFwRw,data, evidenceId,setData,setupdateVideoSelection,updateSeekMarker,gMapApiKey,gpsJson,openMap,setOnMarkerClickTimeData }: VideoScreenProp) => {
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
   const [indexNumber, setIndexNumber] = React.useState<number>(0);
-  const [adjustSoundEnabled, setAdjustSoundEnabled] = React.useState<boolean>(true);
+  const [adjustSoundEnabled, setAdjustSoundEnabled] =
+    React.useState<boolean>(true);
 
   const getVideo = (camIndexVideoData: any) => {
     if (camIndexVideoData !== undefined) {
       return (
         <>
-        <video id={camIndexVideoData.id} width="100%" height="100%" preload="auto" crossOrigin="anonymous">
-          <source src={camIndexVideoData.src} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+          <video
+            id={camIndexVideoData.id}
+            width="100%"
+            height="100%"
+            preload="auto"
+            crossOrigin="anonymous"
+          >
+            <source src={camIndexVideoData.src} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
 
-        <p style={{backgroundColor:"black", color:"white", paddingLeft:"45%"}}>{camIndexVideoData.camera}</p>
+          <p
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              paddingLeft: "45%",
+            }}
+          >
+            {camIndexVideoData.camera}
+          </p>
         </>
-      )
+      );
     } else {
       return <div />;
     }
-  }
+  };
 
   const adjustSound = (enableSound: boolean, id?: any) => {
     var videoHandle = videoHandlers.find((x: any) => x.id == id);
     if (videoHandle !== undefined) {
-      setAdjustSoundEnabled(enableSound)
+      setAdjustSoundEnabled(enableSound);
       if (enableSound) {
         videoHandle.muted = false;
-      }
-      else {
+      } else {
         videoHandle.muted = true;
       }
     }
-  }
+  };
 
   const removeVideo = (indexNumber: any) => {
     var tempArray = [...timelinedetail];
-    var tempItemToRemove = tempArray.find((x: any) => x.indexNumberToDisplay == indexNumber);
+    var tempItemToRemove = tempArray.find(
+      (x: any) => x.indexNumberToDisplay == indexNumber
+    );
     if (tempItemToRemove !== undefined) {
       tempItemToRemove.enableDisplay = false;
       tempItemToRemove.indexNumberToDisplay = 0;
@@ -77,32 +94,71 @@ const VideoScreen = ({onClickBookmarkNote,isPlaying, viewNumber, timelinedetail,
     setIndexNumber(0);
   };
 
+  useEffect(() => {
+    const playBtn = document.getElementById("_video_play");
+    const pauseBtn = document.getElementById("_video_pause")
+
+    if(isPlaying === true) {
+      
+      playBtn?.classList.remove("zoomOut")
+      playBtn?.classList.add("zoomIn")
+      setTimeout(() => {
+        playBtn?.classList.remove("zoomIn")
+        playBtn?.classList.add("zoomOut")
+      },1200);
+
+    }else {
+
+      pauseBtn?.classList.remove("zoomOut")
+      pauseBtn?.classList.add("zoomIn")
+      setTimeout(() => {
+        pauseBtn?.classList.remove("zoomIn")
+        pauseBtn?.classList.add("zoomOut")
+      },1200)
+    
+    }
+   },[isPlaying])
+
   const getVideoTag = (CameraCount: number) => {
     //console.log(viewNumber)
     //poster="https://i.ibb.co/C0PqYN4/Media-Not-Available.png"
-
+    
     let camIndex = CameraCount;
-    var camIndexVideoData = timelinedetail.find((x: any) => x.indexNumberToDisplay == camIndex && x.enableDisplay);
+    var camIndexVideoData = timelinedetail.find(
+      (x: any) => x.indexNumberToDisplay == camIndex && x.enableDisplay
+    );
+
+   
+
     return (
       <div className="videoContainer">
-              <div
-          className="videoFrontLayer">
-          <span className={`alignmentPlayPause ${isPlaying ? "crxAddTransition"  : ""}`} >
-            
+        <div className="videoFrontLayer">
+          
             {isPlaying ? (
-              <div className="crxAlignmentPausePlay crxAlignmentPause">
-              <i className="icon icon-pause2"></i>
+             
+              <div 
+              id="_video_play" 
+              className={`video_play_onScreen _video_button_size animated`}>
+                <PlayButton />
               </div>
+             
             ) : (
-              <div className="crxAlignmentPausePlay crxAlignmentPlay">
-                 <i className="icon icon-play4"></i>
+             
+              <div 
+              id="_video_pause" 
+              className={`video_pause_onScreen _video_button_size animated`}>
+                <PauseButton />
               </div>
+             
             )}
-          </span>
+          
         </div>
         {getVideo(camIndexVideoData)}
 
-        <div className="videoMenuCss" style={{ display: camIndex == 1 ? 'none' : 'block' }}>
+        <div
+          className="videoMenuCss"
+          style={{ display: camIndex == 1 ? "none" : "block" }}
+        >
           <Menu
             align="start"
             viewScroll="initial"
@@ -115,62 +171,89 @@ const VideoScreen = ({onClickBookmarkNote,isPlaying, viewNumber, timelinedetail,
               </MenuButton>
             }
           >
-
-            <MenuItem style={{ visibility: (indexNumber > 0) ? "hidden" : "visible" }}>
-              <div className="crx-meu-content groupingMenu crx-spac" onClick={(event: any) => { setAnchorEl(event.currentTarget); setIndexNumber(camIndex) }}>
+            <MenuItem
+              style={{ visibility: indexNumber > 0 ? "hidden" : "visible" }}
+            >
+              <div
+                className="crx-meu-content groupingMenu crx-spac"
+                onClick={(event: any) => {
+                  setAnchorEl(event.currentTarget);
+                  setIndexNumber(camIndex);
+                }}
+              >
                 <div className="crx-menu-icon"></div>
-                <div className="crx-menu-list">
-                  select video to view
-                </div>
+                <div className="crx-menu-list">select video to view</div>
               </div>
             </MenuItem>
-            <MenuItem >
-              <div className="crx-meu-content groupingMenu crx-spac" onClick={() => removeVideo(camIndex)}>
+            <MenuItem>
+              <div
+                className="crx-meu-content groupingMenu crx-spac"
+                onClick={() => removeVideo(camIndex)}
+              >
                 <div className="crx-menu-icon"></div>
-                <div className="crx-menu-list">
-                  Hide video from view
-                </div>
+                <div className="crx-menu-list">Hide video from view</div>
               </div>
             </MenuItem>
-            <MenuItem >
-              <div className="crx-meu-content groupingMenu crx-spac" >
+            <MenuItem>
+              <div className="crx-meu-content groupingMenu crx-spac">
                 <div className="crx-menu-icon"></div>
                 <div className="crx-menu-list">
-                  <Switch checked={adjustSoundEnabled} onChange={(event) => adjustSound(event.target.checked, camIndexVideoData?.id)} />
+                  <Switch
+                    checked={adjustSoundEnabled}
+                    onChange={(event) =>
+                      adjustSound(event.target.checked, camIndexVideoData?.id)
+                    }
+                  />
                   adjust sound
                 </div>
               </div>
             </MenuItem>
           </Menu>
-
-
         </div>
-
-
       </div>
     );
-
   };
+
   return (
     <div id="video-player-screens">
-
       <Grid container>
         <Grid container xs={mapEnabled ? 9 : 12}>
-
-          <Grid item spacing={3}
-            xs={viewNumber == 1 ? 12 : viewNumber == 2 ? 6 : viewNumber == 3 ? 8 : viewNumber == 4 ? 6 : viewNumber == 6 ? 4 : 1}>
-            {getVideoTag(1)}
+          <Grid
+            item
+            spacing={3}
+            xs={
+              viewNumber == 1
+                ? 12 : viewNumber == 2 ? 6: viewNumber == 3 ? 8: viewNumber == 4 ? 6: viewNumber == 6 ? 4: 1} >
+                  {getVideoTag(1)}
           </Grid>
 
-
-
-          <Grid item spacing={3}
-            xs={viewNumber == 1 ? 1 : viewNumber == 2 ? 6 : viewNumber == 3 ? 4 : viewNumber == 4 ? 6 : viewNumber == 6 ? 4 : 1}>
+          <Grid
+            item
+            spacing={3}
+            xs={
+              viewNumber == 1
+                ? 1
+                : viewNumber == 2
+                ? 6
+                : viewNumber == 3
+                ? 4
+                : viewNumber == 4
+                ? 6
+                : viewNumber == 6
+                ? 4
+                : 1
+            }
+          >
             {getVideoTag(2)}
           </Grid>
-          <Grid item
-            xs={viewNumber == 3 ? 4 : viewNumber == 4 ? 6 : viewNumber == 6 ? 4 : 1}
-            className={`${viewNumber == 2 ? "pictureViewGrid" : ""}`}>
+          <Grid
+            item
+            xs={
+              viewNumber == 3
+                ? 4: viewNumber == 4 ? 6: viewNumber == 6 ? 4: 1
+            }
+            className={`${viewNumber == 2 ? "pictureViewGrid" : ""}`}
+          >
             {getVideoTag(3)}
           </Grid>
           <Grid item xs={viewNumber == 4 ? 6 : viewNumber == 6 ? 4 : 1}>
@@ -188,10 +271,34 @@ const VideoScreen = ({onClickBookmarkNote,isPlaying, viewNumber, timelinedetail,
           <AssetDetailsDropdown data={data} evidenceId={evidenceId} setData={setData}  onClickBookmarkNote={onClickBookmarkNote} updateSeekMarker={updateSeekMarker} gMapApiKey={gMapApiKey} gpsJson={gpsJson} openMap={openMap} setOnMarkerClickTimeData={setOnMarkerClickTimeData} />
         </div>}
       </Grid>
-      <VideosSelection timelinedetail={timelinedetail} settimelinedetail={settimelinedetail} anchorEl={anchorEl} setAnchorEl={setAnchorEl} indexNumber={indexNumber} setupdateVideoSelection={setupdateVideoSelection}/>
-
+      <VideosSelection
+        timelinedetail={timelinedetail}
+        settimelinedetail={settimelinedetail}
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+        indexNumber={indexNumber}
+        setupdateVideoSelection={setupdateVideoSelection}
+      />
     </div>
   );
 };
+
+
+const PlayButton = () => {
+  return (
+    <div className="video_playButton">
+      <i className="icon icon-play4"></i>
+    </div>
+  )
+}
+
+
+const PauseButton = () => {
+  return (
+    <div className="video_PauseButton">
+      <i className="icon icon-pause2"></i>
+    </div>
+  )
+}
 
 export default VideoScreen;
