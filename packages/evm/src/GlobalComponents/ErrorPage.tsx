44ac75d05dc304e71010258fb0243c322e6de75a
bilal shahab <bilal.shahab@hyperdatacomputing.com>
2022-07-19 13:22:45 +0000

    Merged in GEP-1672-404-Not-Found-Page (pull request #412)
    404 page design done
    
    * 404 page design done
    
    * Merge remote-tracking branch 'remotes/origin/development' into GEP-1672-404-Not-Found-Page
    
    * Merged development into GEP-1672-404-Not-Found-Page
    * Merge branch 'GEP-1672-404-Not-Found-Page' of bitbucket.org:irsavideo/crossbones-webui into GEP-1672-404-Not-Found-Page
    
    * Removed duplicate line from css file
    
    * Added font style
    
    
    Approved-by: Sajid Hussain

--- packages/evm/src/GlobalComponents/ErrorPage.tsx
+++ packages/evm/src/GlobalComponents/ErrorPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './ErrorPage.scss'
+
 
 const ErrorPage = () => {
     return (
-        <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',marginTop:"5%"}}>
-           <span style={{fontSize:"300px"}}>404</span> 
-           <span style={{fontSize:"100px"}}>Page Not Found</span> 
+        <div className="errorPageMain">
+           <span className='errorPageHeading'>404 Not Found</span>
+           <span className='errorPageFirst'>Sorry, Something went wrong and we were unable to find <br/>the page you were looking for.</span>
+           <span className='errorPageSecond'>The webpage might be temporarily down or it may have moved permanently <br/>to a new web address.</span>
+           <span className='errorPageSuggestion'>Here are some suggestions:</span>
+           <span >
+            <ul>
+                <li><Link to="/">Reload</Link> this web page later</li>
+                <li>Go back to the <Link to="/">Home page</Link></li>
+            </ul>
+            </span> 
         </div>
     )
 }
