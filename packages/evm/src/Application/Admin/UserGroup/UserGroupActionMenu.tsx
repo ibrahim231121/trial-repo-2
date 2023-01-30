02ed57fb9f6e9379bd7d5348247583d3e09c0a8b
Malaika Khalid <malaika.khalid@hyperdatacomputing.com>
2023-01-30 14:46:43 +0000

    Merged in GEP-1164-Manage-User-Groups-Permissions---Action-icon-menu-is-not-working-on-lister-page (pull request #999)
    GEP-1164 Manage User Groups Permissions   Action icon menu is not working on lister page
    
    * GEP-1164-Manage-User-Groups-Permissions---Action-icon-menu-is-not-working-on-lister-page
    
    * GEP-1164: Action Menu ui issue fixed
    
    * Merged development into GEP-1164-Manage-User-Groups-Permissions---Action-icon-menu-is-not-working-on-lister-page
    * Merged development into GEP-1164-Manage-User-Groups-Permissions---Action-icon-menu-is-not-working-on-lister-page
    * ..
    
    * Merge branch 'GEP-1164-Manage-User-Groups-Permissions---Action-icon-menu-is-not-working-on-lister-page' of https://bitbucket.org/irsavideo/crossbones-webui into GEP-1164-Manage-User-Groups-Permissions---Action-icon-menu-is-not-working-on-lister-page
    
    
    Approved-by: Mohsin Memon

--- packages/evm/src/Application/Admin/UserGroup/UserGroupActionMenu.tsx
+++ packages/evm/src/Application/Admin/UserGroup/UserGroupActionMenu.tsx
@@ -10,6 +10,10 @@ import "@szhsin/react-menu/dist/index.css";
 //import './index.scss'
 import { useDispatch } from "react-redux";
 import { VoidExpression } from "typescript";
+import Restricted from "../../../ApplicationPermission/Restricted";
+import { urlList, urlNames } from "../../../utils/urlList";
+import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 type Props = {
     selectedItems?: any;
@@ -17,24 +21,42 @@ type Props = {
 };
 
 const UserGroupActionMenu: React.FC<Props> = ({ selectedItems, row }) => {
+    const history = useHistory()
+    const { t } = useTranslation<string>();
+    const openCreateUserGroupForm = () => {
+        const path = `${urlList.filter((item:any) => item.name === urlNames.adminUserGroupId)[0].url}`;
+        history.push(path.substring(0, path.lastIndexOf("/")) + "/" + row?.id);
+        
+      };
 
     return (
-        <div className="toolBar">
-            
-        
+        <div className="toolBar"> 
         <Menu
             align="start"
-            viewScroll="initial"
+            viewScroll="close"
             direction="right"
             position="auto"
             className="menuCss"
-            arrow
+            portal={true}
+            offsetX={-19}
+            offsetY={-5}
+            arrow={false}
             menuButton={
                 <MenuButton className="userGroup_lister_editIcon">
-                    <i className="fas fa-pen"></i>
+                    <i className="far fa-ellipsis-v"></i>
                 </MenuButton>
             }
         >
+                <MenuItem onClick={openCreateUserGroupForm}>
+                    <Restricted moduleId={7}>
+                        <div className="crx-meu-content ">
+                            <div className="crx-menu-icon">
+                                <i className="fas fa-pen"></i>
+                            </div>
+                            <div className="crx-menu-list">{t("Edit_user_group")}</div>
+                        </div>
+                    </Restricted>
+                </MenuItem>
         </Menu>
         </div>
     );
