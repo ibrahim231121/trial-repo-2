1f351be2beeea32247ce6b38cc209e6bb9cb0390
muhammad.jawad <muhammad.jawad@hyperdatacomputing.com>
2023-11-02 06:50:06 +0000

    Merged in GEP-9569-Alpr-with-new-communication-service (pull request #41)
    GEP-9569-Alpr with new communication service-env is changes in frontend
    
    * GEP-9569-Alpr with new communication service-env is changes in frontend
    
    
    Approved-by: Mehmood Usman

--- packages/evm/src/env.ts
+++ packages/evm/src/env.ts
@@ -17,8 +17,9 @@ type EnvType = {
   REACT_APP_FILE_SERVICE_URL: string
   REACT_APP_AICOORDINATOR_SERVICE_URL: string
   REACT_APP_CLIENT_ID: string
-  REACT_APP_GOOGLE_MAPS_API_KEY:string
-  REACT_APP_BING_MAPS_API_KEY:string
+
+  REACT_APP_GOOGLE_MAPS_API_KEY: string
+  REACT_APP_BING_MAPS_API_KEY: string
   REACT_APP_AUDITLOG_SERVICE_URL: string
   REACT_APP_CASES_SERVICE_URL: string
   REACT_APP_Configuration_SERVICE_URL: string
@@ -26,6 +27,8 @@ type EnvType = {
   REACT_APP_COMMAND_SERVICE_URL: string
   REACT_APP_SOCKET_SERVICE_URL: string
   REACT_APP_AICOORDINATOR_SERVICE_Redirect_URL: string
+  REACT_APP_CAD_SERVICE_URL: string
+  REACT_APP_ENV: string
   REACT_APP_ALPR_SERVICE_URL: string
 }
 export const env: EnvType = { ...process.env, ...window.env }
--- packages/evm/src/utils/Api/url.ts
+++ packages/evm/src/utils/Api/url.ts
@@ -4,21 +4,42 @@ import { MAX_REQUEST_SIZE_FOR } from '../constant'
 export const BASE_URL_ALPR_Service = env.REACT_APP_ALPR_SERVICE_URL;
 
 
-export const BASE_URL_SOCKET_SERVICE = env.REACT_APP_SOCKET_SERVICE_URL;
-export const BASE_URL_COMMAND_SERVICE = env.REACT_APP_COMMAND_SERVICE_URL;
-const BASE_URL_SEARCH_SERVICE = env.REACT_APP_SEARCH_SERVICE_URL;
-export const BASE_URL_AUTHENTICATION_SERVICE = env.REACT_APP_AUTHENTICATION_SERVICE_URL;
-export const BASE_URL_USER_SERVICE = env.REACT_APP_USER_SERVICE_URL;
-export const BASE_URL_CASES_SERVICE = env.REACT_APP_CASES_SERVICE_URL;
-export const BASE_URL_Configuration_SERVICE = env.REACT_APP_Configuration_SERVICE_URL;
-export const BASE_URL_DeviceHeartBeat_SERVICE = env.REACT_APP_DeviceHeartBeat_SERVICE_URL;
-const BASE_URL_UNIT_SERVICE = env.REACT_APP_UNIT_SERVICE_URL
-const BASE_URL_SETUP_SERVICE = env.REACT_APP_SETUP_SERVICE_URL
-const REACT_APP_EVIDENCE_SERVICE_URL = env.REACT_APP_EVIDENCE_SERVICE_URL
-const REACT_APP_FILE_SERVICE_URL = env.REACT_APP_FILE_SERVICE_URL
-export const AUDITLOG_SERVICE_URL= env.REACT_APP_AUDITLOG_SERVICE_URL
-const REACT_APP_AICOORDINATOR_SERVICE_URL = env.REACT_APP_AICOORDINATOR_SERVICE_URL
-export const REDIRECT_URL_AICOORDINATOR = env.REACT_APP_AICOORDINATOR_SERVICE_Redirect_URL
+const hostUrl:any = window.location.protocol + "//" + window.location.hostname;
+const baseApi:any = window.location.protocol + "//" + window.location.hostname  + "/api/v1";
+const isDevelopment : any = env.REACT_APP_ENV
+export const BASE_URL_SOCKET_SERVICE = isDevelopment == "development" ? env.REACT_APP_SOCKET_SERVICE_URL : window.location.protocol + "//" + window.location.hostname + "/crossbone";
+export const BASE_URL_COMMAND_FRONTEND = isDevelopment == "development" ? env.REACT_APP_COMMAND_SERVICE_URL : hostUrl + "/cmd";
+export const BASE_URL_COMMAND_SERVICE = isDevelopment == "development" ? env.REACT_APP_COMMAND_SERVICE_URL : baseApi;
+const BASE_URL_SEARCH_SERVICE = isDevelopment == "development" ? env.REACT_APP_SEARCH_SERVICE_URL : baseApi;
+export const BASE_URL_AUTHENTICATION_SERVICE = isDevelopment == "development" ? env.REACT_APP_AUTHENTICATION_SERVICE_URL : hostUrl;
+export const BASE_URL_USER_SERVICE = isDevelopment == "development" ? env.REACT_APP_USER_SERVICE_URL : baseApi;
+export const BASE_URL_CASES_SERVICE = isDevelopment == "development" ? env.REACT_APP_CASES_SERVICE_URL : baseApi;
+export const BASE_URL_Configuration_SERVICE = isDevelopment == "development" ? env.REACT_APP_Configuration_SERVICE_URL : baseApi;
+export const BASE_URL_DeviceHeartBeat_SERVICE = isDevelopment == "development" ? env.REACT_APP_DeviceHeartBeat_SERVICE_URL : baseApi  + "/auth";
+const BASE_URL_UNIT_SERVICE = isDevelopment == "development" ? env.REACT_APP_UNIT_SERVICE_URL : baseApi
+const BASE_URL_SETUP_SERVICE = isDevelopment == "development" ? env.REACT_APP_SETUP_SERVICE_URL : baseApi
+const REACT_APP_EVIDENCE_SERVICE_URL = isDevelopment == "development" ? env.REACT_APP_EVIDENCE_SERVICE_URL : baseApi
+const REACT_APP_FILE_SERVICE_URL = isDevelopment == "development" ? env.REACT_APP_FILE_SERVICE_URL : baseApi
+export const AUDITLOG_SERVICE_URL= isDevelopment == "development" ? env.REACT_APP_AUDITLOG_SERVICE_URL : baseApi
+const REACT_APP_AICOORDINATOR_SERVICE_URL = isDevelopment == "development" ? env.REACT_APP_AICOORDINATOR_SERVICE_URL : baseApi
+export const REDIRECT_URL_AICOORDINATOR = isDevelopment == "development" ? env.REACT_APP_AICOORDINATOR_SERVICE_Redirect_URL : baseApi
+export const BASE_URL_CAD_FRONTEND = isDevelopment == "development" ? env.REACT_APP_CAD_SERVICE_URL : hostUrl + "/cad";
+export const BASE_URL_CAD_SERVICE = isDevelopment == "development" ? env.REACT_APP_CAD_SERVICE_URL : baseApi;
+// export const BASE_URL_SOCKET_SERVICE = env.REACT_APP_SOCKET_SERVICE_URL;
+// export const BASE_URL_COMMAND_SERVICE = env.REACT_APP_COMMAND_SERVICE_URL;
+// const BASE_URL_SEARCH_SERVICE = env.REACT_APP_SEARCH_SERVICE_URL;
+// export const BASE_URL_AUTHENTICATION_SERVICE = env.REACT_APP_AUTHENTICATION_SERVICE_URL;
+// export const BASE_URL_USER_SERVICE = env.REACT_APP_USER_SERVICE_URL;
+// export const BASE_URL_CASES_SERVICE = env.REACT_APP_CASES_SERVICE_URL;
+// export const BASE_URL_Configuration_SERVICE = env.REACT_APP_Configuration_SERVICE_URL;
+// export const BASE_URL_DeviceHeartBeat_SERVICE = env.REACT_APP_DeviceHeartBeat_SERVICE_URL;
+// const BASE_URL_UNIT_SERVICE = env.REACT_APP_UNIT_SERVICE_URL
+// const BASE_URL_SETUP_SERVICE = env.REACT_APP_SETUP_SERVICE_URL
+// const REACT_APP_EVIDENCE_SERVICE_URL = env.REACT_APP_EVIDENCE_SERVICE_URL
+// const REACT_APP_FILE_SERVICE_URL = env.REACT_APP_FILE_SERVICE_URL
+// export const AUDITLOG_SERVICE_URL= env.REACT_APP_AUDITLOG_SERVICE_URL
+// const REACT_APP_AICOORDINATOR_SERVICE_URL = env.REACT_APP_AICOORDINATOR_SERVICE_URL
+// export const REDIRECT_URL_AICOORDINATOR = env.REACT_APP_AICOORDINATOR_SERVICE_Redirect_URL
 
 export const EVIDENCE_PREDITIVE_URL = `${BASE_URL_SEARCH_SERVICE}/Evidence/predictive`
 
@@ -28,13 +49,13 @@ export const EVIDENCE_GET_BY_ID_URL = `${BASE_URL_SEARCH_SERVICE}/Evidence`
 
 export const EVIDENCE_SEARCH_VERSION_URL = `${BASE_URL_SEARCH_SERVICE}/Evidence/Version`
 
-export const AUTHENTICATION_NewAccessToken_URL = `${BASE_URL_AUTHENTICATION_SERVICE}/Authentication/GetAccessToken`
+export const AUTHENTICATION_NewAccessToken_URL = `${BASE_URL_AUTHENTICATION_SERVICE}/Auth/Authentication/GetAccessToken`
 
-export const AUTHENTICATION_LOGIN_URL = `${BASE_URL_AUTHENTICATION_SERVICE}/Authentication/Login`
+export const AUTHENTICATION_LOGIN_URL = `${BASE_URL_AUTHENTICATION_SERVICE}/Auth/Authentication/Login`
 
-export const AUTHENTICATION_CODEVERIFIER_URL = `${BASE_URL_AUTHENTICATION_SERVICE}/Authentication/CodeVerifier`
+export const AUTHENTICATION_CODEVERIFIER_URL = `${BASE_URL_AUTHENTICATION_SERVICE}/Auth/Authentication/CodeVerifier`
 
-export const AUTHENTICATION_EMAIL_SERVICE = `${BASE_URL_AUTHENTICATION_SERVICE}/Authentication/ActivateUser`
+export const AUTHENTICATION_EMAIL_SERVICE = `${BASE_URL_AUTHENTICATION_SERVICE}/Auth/Authentication/ActivateUser`
 
 export const GROUP_GET_URL = `${BASE_URL_USER_SERVICE}/groups?Page=1&Size=500`
 
@@ -95,9 +116,10 @@ export const UPLOAD_POLICIES_GET_ALL_DATA = `${BASE_URL_SETUP_SERVICE}/Policies/
 export const UPLOAD_POLICIES = `${BASE_URL_SETUP_SERVICE}/Policies/UpsertUploadPolicy`;
 export const UPLOAD_POLICIES_GET_ALL_UPLOAD_POLICIES_DATA = `${SETUP_CONFIGURATION_SERVICE_URL}/Policies/GetAllUploadPolicies`;
 
+export const REACT_APP_CLIENT_ID = env.REACT_APP_CLIENT_ID
+export const REACT_APP_GOOGLE_MAPS_API_KEY = env.REACT_APP_GOOGLE_MAPS_API_KEY
+
 export const ALPR_HOTLIST = `${BASE_URL_ALPR_Service}/HotList`
 
 export const ALPR_CAPTURE_PLATE = `${BASE_URL_ALPR_Service}/CapturedPlate`
 export const ALPR_PLATE_HISTORY = `${BASE_URL_ALPR_Service}/LicensePlate/{numberPlateId}/history`
-
-export const REACT_APP_CLIENT_ID = env.REACT_APP_CLIENT_ID
